a57ceb242f6c7406102d609e1616c0fd7c73f141
Guido Pawluk <guidopawluk@gmail.com>
2026-06-02 11:59:09 -0300

    fix del sincronizador

--- app/Console/Commands/SyncMetricoolMonthly.php
+++ app/Console/Commands/SyncMetricoolMonthly.php
@@ -47,6 +47,7 @@ class SyncMetricoolMonthly extends Command
                 $job->handle(
                     app(\App\Services\Metricool\MetricoolBundleBuilder::class),
                     app(\App\Services\Metricool\KpiCalculator::class),
+                    app(\App\Services\GoogleAdsService::class),
                 );
             } else {
                 dispatch($job);
