9d196fda675248184633cfa55dffea7380b77f18
Guido Pawluk <guidopawluk@gmail.com>
2026-06-19 12:41:54 -0300

    fix de redirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Inertia\Inertia;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -34,7 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+        $exceptions->respond(function (\Symfony\Component\HttpFoundation\Response $response, Throwable $exception, Request $request) {
             $status = $response->getStatusCode();
 
             if (in_array($status, [403, 404, 500, 503])) {
